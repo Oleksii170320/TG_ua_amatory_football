ffe82c6c82b8387f2461c9333f22bf922ee3be9f
Oleksii <celect1989@gmail.com>
2024-10-23 12:04:20 +0300

    third stap

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import asyncio
 import logging
-import os
-from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher
 
 from app.handlers import router
 from app.database.models import async_main
+from config import TOKEN
 
 
 async def main():
     await async_main()
-    load_dotenv()
-    bot = Bot(token=os.getenv('TOKEN'))
+    bot = Bot(token=TOKEN)
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
