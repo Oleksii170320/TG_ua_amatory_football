3b37f99454e29a6a676d98fd960570dfaf22e4ab
Oleksii <celect1989@gmail.com>
2024-10-23 11:57:22 +0300

    Second stap

---
+++ app/database/database.py
@@ -0,0 +1,5 @@
+import sqlite3 as sq
+
+
+db = sq.connect('tg.db')
+cur = db.cursor()
---
+++ app/database/regions_list.py
@@ -0,0 +1,127 @@
+regions = {
+    "Vinnytska": {
+        "name": "Вінницька область",
+        "category": "Футбол Вінницької області",
+        "website": "http://vinoff.com.ua/"
+    },
+    "Volynsk": {
+        "name": "Волинська область",
+        "category": "Футбол Волинської області",
+        "website": "https://ffv.org.ua/"
+    },
+    "Dnipropetrovska": {
+        "name": "Дніпропетровська область",
+        "category": "Футбол Дніпропетровської області",
+        "website": "https://afdo.com.ua/"
+    },
+    "Donetska": {
+        "name": "Донецька область",
+        "category": "Футбол Донецької області",
+        "website": "http://ffdo.dn.ua/"
+    },
+    "Zhytomyrska": {
+        "name": "Житомирська область",
+        "category": "Футбол Житомирської області",
+        "website": "https://www.zhoff.org.ua/"
+    },
+    "Zakarpatskа": {
+        "name": "Закарпатська область",
+        "category": "Футбол Закарпатської області",
+        "website": "https://ffz.org.ua/"
+    },
+    "Zaporizka": {
+        "name": "Запоріжська область",
+        "category": "Футбол Запоріжської області",
+        "website": "http://zoff.zp.ua/"
+    },
+    "Ivano-Frankivska": {
+        "name": "Івано-Франківська область",
+        "category": "Футбол Івано-Франківської області",
+        "website": "http://www.aafu.org.ua/regions"
+    },
+    "Kyivska": {
+        "name": "Київська область",
+        "category": "Футбол Київської області",
+        "website": "https://koaf.com.ua/"
+    },
+    "Kirovohradska": {
+        "name": "Кіровоградська область",
+        "category": "Футбол Кіровоградської області",
+        "website": "https://koff.com.ua/"
+    },
+    "Luhanska": {
+        "name": "Луганська область",
+        "category": "Футбол Луганської області",
+        "website": "http://laf.kl.com.ua/"
+    },
+    "Lvivska": {
+        "name": "Львівська область",
+        "category": "Футбол Львівської області",
+        "website": "https://ffl.org.ua/"
+    },
+    "Mykolayivska": {
+        "name": "Миколаївська область",
+        "category": "Футбол Миколаївської області",
+        "website": "https://ffmo.com.ua/"
+    },
+    "Odeska": {
+        "name": "Одеська область",
+        "category": "Футбол Одеської області",
+        "website": "https://www.oofa.com.ua/"
+    },
+    "Poltavska": {
+        "name": "Полтавська область",
+        "category": "Футбол Полтавської області",
+        "website": "https://ffp.org.ua/"
+    },
+    "Rivnenska": {
+        "name": "Рівненська область",
+        "category": "Футбол Рівненської області",
+        "website": "https://roaf.rv.ua/"
+    },
+    "Sumska": {
+        "name": "Сумська область",
+        "category": "Футбол Сумської області",
+        "website": "https://ffs.sumy.ua/"
+    },
+    "Ternopilska": {
+        "name": "Тернопільська область",
+        "category": "Футбол Тернопільської області",
+        "website": "https://aft.com.ua/"
+    },
+    "Kharkivska": {
+        "name": "Харківська область",
+        "category": "Футбол Харківської області",
+        "website": "https://www.hoff.org.ua/"
+    },
+    "Khersonska": {
+        "name": "Херсонська область",
+        "category": "Футбол Херсонської області",
+        "website": "https://hoff.ks.ua/"
+    },
+    "Khmelnytska": {
+        "name": "Хмельницька область",
+        "category": "Футбол Хмельницької області",
+        "website": "https://hoff.km.ua/"
+    },
+    "Cherkaska": {
+        "name": "Черкаська область",
+        "category": "Футбол Черкаської області",
+        "website": "http://ffcho.org.ua/"
+    },
+    "Chernivetska": {
+        "name": "Чернівецька область",
+        "category": "Футбол Чернівецької області",
+        "website": "https://ffbuk.org.ua/"
+    },
+    "Chernihivska": {
+        "name": "Чернігівська область",
+        "category": "Футбол Чернігівської області",
+        "website": "http://www.cnoff.com.ua/"
+    },
+    "Kyiv": {
+        "name": "місто Київ",
+        "category": "Футбол міста Київ",
+        "website": "https://ffk.kiev.ua/"
+    }
+}
